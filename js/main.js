a072b52043ef741d955767638a46974334dc30fd
barbahamaria <barbaha.maria@gmail.com>
2016-06-19 22:47:14 -0300

    retirando códigos e arquivos desnecessários

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-//////////////////// FUNÇÃO PARA O BANNER ////////////////////////
-// nessa função determinamos as coisas que serão usadas no banner, 
-// apontamos a classe ou id que determinamos para o banner, no nosso 
-// caso slider, e chamamos a função cycle dentro dela determinamos o 
-// tempo(timeout) para a passagem das imagens, qual será o tipo da transição 
-// das imagens com o fx, e onde é colocado o indice(pager) que nonosso 
-// caso é o span que tem a classe pagina e em seguida foi criado uma ancora 
-// para adicionar a tag a ao span e depois, dizemos qual é o nome da classe 
-// para ser usada quando o indice apontar a imagem(activePagerClass)
+
 $(function(){
 	$('.slider').cycle({
 		timeout: 3000,
@@ -19,158 +11,146 @@ $(function(){
 	});
 }); 
 
-//////////////// FUNÇÃO DO BANNER FINALIZADA ///////////////////////
 
-$(function () { // wait for document ready
-	// init
+//__________________________________________________________________
+
+
+
+$(function () {
 	var controller = new ScrollMagic.Controller({
 		globalSceneOptions: {
 			triggerHook: 'onLeave'
 		}
 	});
 
-	// get all slides
 	var slides = document.querySelectorAll("section.movit");
 
-	// create scene for every slide
 	for (var i=0; i<slides.length; i++) {
 		new ScrollMagic.Scene({
 			triggerElement: slides[i]
 		})
 		.setPin(slides[i])
-		.addIndicators() // add indicators (requires plugin)
+		.addIndicators()
 		.addTo(controller);
 	}
 });
 
 
 
-// __________________________________________________________________
-
+//__________________________________________________________________
 
 
 function MascaraCep(cep){
-                if(mascaraInteiro()==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(cep, '00.000-000', event);
+    if(mascaraInteiro()==false){
+        event.returnValue = false;
+    }       
+    return formataCampo(cep, '00.000-000', event);
 }
 
-//adiciona mascara de data
 function MascaraData(data){
-        if(mascaraInteiro(data)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(data, '00/00/0000', event);
+    if(mascaraInteiro(data)==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(data, '00/00/0000', event); 
 }
 
-//adiciona mascara ao telefone
 function MascaraTelefone(tel){  
-        if(mascaraInteiro(tel)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(tel, '(00) 00000-0000', event);
+    if(mascaraInteiro(tel)==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(tel, '(00) 00000-0000', event); 
 }
 
-//adiciona mascara ao CPF
 function MascaraCPF(cpf){
-        if(mascaraInteiro(cpf)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(cpf, '000.000.000-00', event);
+    if(mascaraInteiro(cpf)==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(cpf, '000.000.000-00', event);
 }
 
-//adiciona mascara ao RG
 function MascaraRG(rg){
-        if((rg)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(rg, '00.000.000-0', event);
+    if((rg)==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(rg, '00.000.000-0', event); 
 }
 
-//valida telefone
 function ValidaTelefone(tel){
-        exp = /\(\d{2}\)\ \d{4}\-\d{4}/
-        if(!exp.test(tel.value))
-                alert('Numero de Telefone Invalido!');
+    exp = /\(\d{2}\)\ \d{4}\-\d{4}/
+    if(!exp.test(tel.value))
+            alert('Numero de Telefone Invalido!');  
 }
 
-//valida CEP
 function ValidaCep(cep){
-        exp = /\d{2}\.\d{3}\-\d{3}/
-        if(!exp.test(cep.value))
-                alert('Numero de Cep Invalido!');               
+    exp = /\d{2}\.\d{3}\-\d{3}/
+    if(!exp.test(cep.value))
+            alert('Numero de Cep Invalido!');               
 }
 
-//valida data
 function ValidaData(data){
-        exp = /\d{2}\/\d{2}\/\d{4}/
-        if(!exp.test(data.value))
-                alert('Data Invalida!');                        
+    exp = /\d{2}\/\d{2}\/\d{4}/
+    if(!exp.test(data.value))
+            alert('Data Invalida!');                        
 }
 
-//valida o CPF digitado
 function ValidarCPF(Objcpf){
-        var cpf = Objcpf.value;
-        exp = /\.|\-/g
-        cpf = cpf.toString().replace( exp, "" ); 
-        var digitoDigitado = eval(cpf.charAt(9)+cpf.charAt(10));
-        var soma1=0, soma2=0;
-        var vlr =11;
+    var cpf = Objcpf.value;
+    exp = /\.|\-/g
+    cpf = cpf.toString().replace( exp, "" ); 
+    var digitoDigitado = eval(cpf.charAt(9)+cpf.charAt(10));
+    var soma1=0, soma2=0;
+    var vlr =11;
 
-        for(i=0;i<9;i++){
-                soma1+=eval(cpf.charAt(i)*(vlr-1));
-                soma2+=eval(cpf.charAt(i)*vlr);
-                vlr--;
-        }       
-        soma1 = (((soma1*10)%11)==10 ? 0:((soma1*10)%11));
-        soma2=(((soma2+(2*soma1))*10)%11);
+    for(i=0;i<9;i++){
+            soma1+=eval(cpf.charAt(i)*(vlr-1));
+            soma2+=eval(cpf.charAt(i)*vlr);
+            vlr--;
+    }       
+    soma1 = (((soma1*10)%11)==10 ? 0:((soma1*10)%11));
+    soma2=(((soma2+(2*soma1))*10)%11);
 
-        var digitoGerado=(soma1*10)+soma2;
-        if(digitoGerado!=digitoDigitado)        
-                alert('CPF Invalido!');         
+    var digitoGerado=(soma1*10)+soma2;
+    if(digitoGerado!=digitoDigitado)        
+            alert('CPF Invalido!');         
 }
 
-//valida numero inteiro com mascara
 function mascaraInteiro(){
-        var e = event.keyCode;
-        if (e < 48 || e > 57){
-                event.returnValue = false;
-                return false;
-        }
-        return true;
+    var e = event.keyCode;
+    if (e < 48 || e > 57){
+            event.returnValue = false;
+            return false;
+    }
+    return true;
 }
 
 
-//formata de forma generica os campos
 function formataCampo(campo, Mascara, evento) { 
-        var boleanoMascara; 
+    var boleanoMascara; 
 
-        var Digitato = evento.keyCode;
-        exp = /\-|\.|\/|\(|\)| /g
-        campoSoNumeros = campo.value.toString().replace( exp, "" ); 
+    var Digitato = evento.keyCode;
+    exp = /\-|\.|\/|\(|\)| /g
+    campoSoNumeros = campo.value.toString().replace( exp, "" ); 
 
-        var posicaoCampo = 0;    
-        var NovoValorCampo="";
-        var TamanhoMascara = campoSoNumeros.length;; 
+    var posicaoCampo = 0;    
+    var NovoValorCampo="";
+    var TamanhoMascara = campoSoNumeros.length;; 
 
-        if (Digitato != 8) { // backspace 
-                for(i=0; i<= TamanhoMascara; i++) { 
-                        boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
-                                                                || (Mascara.charAt(i) == "/")) 
-                        boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") 
-                                                                || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " ")) 
-                        if (boleanoMascara) { 
-                                NovoValorCampo += Mascara.charAt(i); 
-                                  TamanhoMascara++;
-                        }else { 
-                                NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
-                                posicaoCampo++; 
-                          }              
-                  }      
-                campo.value = NovoValorCampo;
-                  return true; 
-        }else { 
-                return true; 
-        }
+            for(i=0; i<= TamanhoMascara; i++) { 
+                    boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
+                                                            || (Mascara.charAt(i) == "/")) 
+                    boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") 
+                                                            || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " ")) 
+                    if (boleanoMascara) { 
+                            NovoValorCampo += Mascara.charAt(i); 
+                              TamanhoMascara++;
+                    }else { 
+                            NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
+                            posicaoCampo++; 
+                      }              
+              }      
+            campo.value = NovoValorCampo;
+              return true; 
+    }else { 
+            return true; 
+    }
 }
--- js/validacao.js
+++ js/validacao.js
@@ -1,129 +1,114 @@
-// Foi criado um arquivo apenas para a validação para não precisar utilizar
-// arquivos desnecessários em algumas páginas.
-
-
 function MascaraCep(cep){
-                if(mascaraInteiro()==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(cep, '00.000-000', event);
+            if(mascaraInteiro()==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(cep, '00.000-000', event);
 }
 
-//adiciona mascara de data
 function MascaraData(data){
-        if(mascaraInteiro(data)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(data, '00/00/0000', event);
+    if(mascaraInteiro(data)==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(data, '00/00/0000', event);
 }
 
-//adiciona mascara ao telefone
 function MascaraTelefone(tel){  
-        if(mascaraInteiro(tel)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(tel, '(00) 00000-0000', event);
+    if(mascaraInteiro(tel)==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(tel, '(00) 00000-0000', event);
 }
 
-//adiciona mascara ao CPF
 function MascaraCPF(cpf){
-        if(mascaraInteiro(cpf)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(cpf, '000.000.000-00', event);
+    if(mascaraInteiro(cpf)==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(cpf, '000.000.000-00', event);
 }
 
-//adiciona mascara ao RG
 function MascaraRG(rg){
-        if((rg)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(rg, '00.000.000-0', event);
+    if((rg)==false){
+            event.returnValue = false;
+    }       
+    return formataCampo(rg, '00.000.000-0', event);
 }
 
-//valida telefone
 function ValidaTelefone(tel){
-        exp = /\(\d{2}\)\ \d{4}\-\d{4}/
-        if(!exp.test(tel.value))
-                alert('Numero de Telefone Invalido!');
+    exp = /\(\d{2}\)\ \d{4}\-\d{4}/
+    if(!exp.test(tel.value))
+            alert('Numero de Telefone Invalido!');
 }
 
-//valida CEP
 function ValidaCep(cep){
-        exp = /\d{2}\.\d{3}\-\d{3}/
-        if(!exp.test(cep.value))
-                alert('Numero de Cep Invalido!');               
+    exp = /\d{2}\.\d{3}\-\d{3}/
+    if(!exp.test(cep.value))
+            alert('Numero de Cep Invalido!');               
 }
 
-//valida data
 function ValidaData(data){
-        exp = /\d{2}\/\d{2}\/\d{4}/
-        if(!exp.test(data.value))
-                alert('Data Invalida!');                        
+    exp = /\d{2}\/\d{2}\/\d{4}/
+    if(!exp.test(data.value))
+            alert('Data Invalida!');                        
 }
 
-//valida o CPF digitado
 function ValidarCPF(Objcpf){
-        var cpf = Objcpf.value;
-        exp = /\.|\-/g
-        cpf = cpf.toString().replace( exp, "" ); 
-        var digitoDigitado = eval(cpf.charAt(9)+cpf.charAt(10));
-        var soma1=0, soma2=0;
-        var vlr =11;
+    var cpf = Objcpf.value;
+    exp = /\.|\-/g
+    cpf = cpf.toString().replace( exp, "" ); 
+    var digitoDigitado = eval(cpf.charAt(9)+cpf.charAt(10));
+    var soma1=0, soma2=0;
+    var vlr =11;
 
-        for(i=0;i<9;i++){
-                soma1+=eval(cpf.charAt(i)*(vlr-1));
-                soma2+=eval(cpf.charAt(i)*vlr);
-                vlr--;
-        }       
-        soma1 = (((soma1*10)%11)==10 ? 0:((soma1*10)%11));
-        soma2=(((soma2+(2*soma1))*10)%11);
+    for(i=0;i<9;i++){
+            soma1+=eval(cpf.charAt(i)*(vlr-1));
+            soma2+=eval(cpf.charAt(i)*vlr);
+            vlr--;
+    }       
+    soma1 = (((soma1*10)%11)==10 ? 0:((soma1*10)%11));
+    soma2=(((soma2+(2*soma1))*10)%11);
 
-        var digitoGerado=(soma1*10)+soma2;
-        if(digitoGerado!=digitoDigitado)        
-                alert('CPF Invalido!');         
+    var digitoGerado=(soma1*10)+soma2;
+    if(digitoGerado!=digitoDigitado)        
+    
+    alert('CPF Invalido!');         
 }
 
-//valida numero inteiro com mascara
 function mascaraInteiro(){
-        var e = event.keyCode;
-        if (e < 48 || e > 57){
-                event.returnValue = false;
-                return false;
-        }
-        return true;
+    var e = event.keyCode;
+    if (e < 48 || e > 57){
+            event.returnValue = false;
+            return false;
+    }
+    return true;
 }
 
-
-//formata de forma generica os campos
 function formataCampo(campo, Mascara, evento) { 
-        var boleanoMascara; 
+    var boleanoMascara; 
 
-        var Digitato = evento.keyCode;
-        exp = /\-|\.|\/|\(|\)| /g
-        campoSoNumeros = campo.value.toString().replace( exp, "" ); 
+    var Digitato = evento.keyCode;
+    exp = /\-|\.|\/|\(|\)| /g
+    campoSoNumeros = campo.value.toString().replace( exp, "" ); 
 
-        var posicaoCampo = 0;    
-        var NovoValorCampo="";
-        var TamanhoMascara = campoSoNumeros.length;; 
+    var posicaoCampo = 0;    
+    var NovoValorCampo="";
+    var TamanhoMascara = campoSoNumeros.length;; 
 
-        if (Digitato != 8) { // backspace 
-                for(i=0; i<= TamanhoMascara; i++) { 
-                        boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
-                                                                || (Mascara.charAt(i) == "/")) 
-                        boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") 
-                                                                || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " ")) 
-                        if (boleanoMascara) { 
-                                NovoValorCampo += Mascara.charAt(i); 
-                                  TamanhoMascara++;
-                        }else { 
-                                NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
-                                posicaoCampo++; 
-                          }              
-                  }      
-                campo.value = NovoValorCampo;
-                  return true; 
-        }else { 
-                return true; 
-        }
+        for(i=0; i<= TamanhoMascara; i++) { 
+            boleanoMascara  = ((Mascara.charAt(i) == "-") || (Mascara.charAt(i) == ".")
+                || (Mascara.charAt(i) == "/")) 
+            boleanoMascara  = boleanoMascara || ((Mascara.charAt(i) == "(") 
+                || (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " ")) 
+            if (boleanoMascara) { 
+                NovoValorCampo += Mascara.charAt(i); 
+                TamanhoMascara++;
+            }else { 
+                NovoValorCampo += campoSoNumeros.charAt(posicaoCampo); 
+                posicaoCampo++; 
+            }              
+        }      
+        campo.value = NovoValorCampo;
+        return true; 
+    }else { 
+        return true; 
+    }
 }
